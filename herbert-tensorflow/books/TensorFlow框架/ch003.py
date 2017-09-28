4d92a1b8a0bae232e8d91aafa05ea550b84e2055
herbert <herbert_git@163.com>
2017-09-28 16:41:53 +0800

    rename dir name

---
+++ herbert-tensorflow/books/TensorFlow框架/ch003.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 
+@author: Herbert
+@license: Apache Licence 
+@contact: 
+@site: 
+@software: PyCharm Community Edition
+@file: ch003.py
+@time: 17-9-28 下午3:51
+"""
+
+
+def func():
+    pass
+
+
+if __name__ == '__main__':
+    pass
