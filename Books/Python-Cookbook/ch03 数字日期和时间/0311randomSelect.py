85be22866e7a6fa8d49c230e02710a5800d26afb
herbert <herbert_git@163.com>
2017-09-29 08:48:35 +0800

    dir change++

---
+++ Books/Python-Cookbook/ch03 数字日期和时间/0311randomSelect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 
+@author: Herbert
+@license: Apache Licence 
+@contact: 
+@site: 
+@software: PyCharm Community Edition
+@file: 0311randomSelect.py
+@time: 17-9-28 下午5:57
+"""
+
+
