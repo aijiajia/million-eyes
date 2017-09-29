63f792d85993c1eb3036f9501191323b58bd9355
Herbert <herbert_git@163.com>
2017-09-29 13:41:31 +0800

    PEP8

--- Books/TensorFlow框架/ch03-helloworld.py
+++ Books/TensorFlow框架/ch03-helloworld.py
@@ -32,11 +32,7 @@ import numpy as np
 # with tf.Session() as sess:
 #     print(sess.run(result))
 
-
-
-#3.4.3 神经网络参数与TensorFlow变量
-
-
+# 3.4.3 神经网络参数与TensorFlow变量
 # #声明w1 w2 两个变量。这里还通过seed参数设定了随机种子
 # #这样可以保证每次运行得到结果是一样的
 #
@@ -45,11 +41,11 @@ w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
 w3 = tf.Variable(tf.truncated_normal([2], stddev=0.1))
 b2 = tf.Variable(tf.constant(0.1, shape=[10]))
 
-#暂时将输入定义一个常量。1*2 的矩阵
+# 暂时将输入定义一个常量。1*2 的矩阵
 
 x = tf.constant([[0.7,0.9]])
 
-#计算神经网络的输出
+# 计算神经网络的输出
 a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
 # page55
