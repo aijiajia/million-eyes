85129227cbaa52e9bbf28b4d9fee3404f10a21c5
Herbert <herbert_git@163.com>
2017-10-09 14:05:34 +0800

    0103保存最后N个元素

--- Books/Python-Cookbook/ch01数据结构和算法/0103保存最后N个元素.py
+++ Books/Python-Cookbook/ch01数据结构和算法/0103保存最后N个元素.py
@@ -13,10 +13,26 @@
 @time: 2017/10/8 9:45
 """
 
+# 问题： 我们希望在迭代或是其他形式的处理过程中，对最后几项记录做一个有限的历史记录
+# 解决： collections.deque
+from collections import deque
 
-def func():
-    pass
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
 
 
 if __name__ == '__main__':
-    pass
+    # text = 'hello world, python'
+    # if 'python' in text:
+    #     print('-'*20)
+    with open('somefile.txt') as f:
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-'*20)
