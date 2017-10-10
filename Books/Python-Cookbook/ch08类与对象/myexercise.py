58fabc59e3c6c1bd35bc13d08960775a1a36aa44
Herbert <herbert_git@163.com>
2017-10-10 14:28:21 +0800

    0806创建可管理的属性

---
+++ Books/Python-Cookbook/ch08类与对象/myexercise.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 
+@author: Herbert
+@license: Apache Licence 
+@contact: 
+@site: 
+@software: PyCharm Community Edition
+@file: myexercise.py
+@time: 2017/10/10 14:20
+"""
+
+
+class Person:
+    def __init__(self, first_name):
+        self.first_name = first_name
+
+    # Getter function
+    @property
+    def first_name(self):
+        return self._first_name
+
+    # Setter function
+    @first_name.setter
+    def first_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Expected a string')
+        self._first_name = value
+
+    # Deleter function (optional)
+    @first_name.deleter
+    def first_name(self):
+        raise AttributeError("Can not delte attribute!")
+
+
+if __name__ == '__main__':
+    a = Person('Herbert')
+    print(a.first_name)
