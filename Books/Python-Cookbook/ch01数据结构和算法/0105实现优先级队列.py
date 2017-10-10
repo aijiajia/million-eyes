fee4650a55b8a1b68bd52c02cb0ff0d5abbbfee3
Herbert <herbert_git@163.com>
2017-10-10 08:38:48 +0800

    0105实现优先级队列

--- Books/Python-Cookbook/ch01数据结构和算法/0105实现优先级队列.py
+++ Books/Python-Cookbook/ch01数据结构和算法/0105实现优先级队列.py
@@ -12,11 +12,43 @@
 @file: 0105实现优先级队列.py
 @time: 2017/10/8 9:45
 """
+# 问题：想要实现一个队列，它能够以给定的优先级来对元素排序，且每次pop操作时都会返回优先级最高的元素。
+# 解决：heapq;
+# 优先级相同时，返回顺序按照插入顺序
+import heapq
 
 
-def func():
-    pass
+class PriorityQueue:
+
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+    def size(self):
+        return len(self._queue)
+
+
+class Item:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        # !r 为占位内容添加单引号包裹
+        return 'Item({!r})'.format(self.name)
 
 
 if __name__ == '__main__':
-    pass
+    q = PriorityQueue()
+    q.push(Item('foo'), 1)
+    q.push(Item('bar'), 4)
+    q.push(Item('spam'), 1)
+    for i in range(q.size()):
+        print(q.pop())
