b224f95b35cc35f75e88a66b3d1febcda9627a77
Herbert <herbert_git@163.com>
2017-10-13 11:52:17 +0800

    矩阵

---
+++ Books/Python-Cookbook/ch05文件和IO/0512检测文件是否存在.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 
+@author: Herbert
+@license: Apache Licence 
+@contact: 
+@site: 
+@software: PyCharm Community Edition
+@file: 0512检测文件是否存在.py
+@time: 2017/10/10 10:00
+"""
+
+
+def func():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ Books/TensorFlow框架/ch03完整的例子.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 
+@author: Herbert
+@license: Apache Licence 
+@contact: 
+@site: 
+@software: PyCharm Community Edition
+@file: ch03完整的例子.py
+@time: 2017/10/6 12:26
+"""
+
+
+def func():
+    pass
+
+
+if __name__ == '__main__':
+    pass
