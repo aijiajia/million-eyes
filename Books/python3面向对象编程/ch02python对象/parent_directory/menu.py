eb2df744e861a4ff7dd159bfdf6befb445f1dfc1
wuyueshan27 <herbert_git@163.com>
2018-06-01 18:10:48 +0800

    搞定了bug

--- Books/python3面向对象编程/ch02python对象/parent_directory/menu.py
+++ Books/python3面向对象编程/ch02python对象/parent_directory/menu.py
@@ -20,14 +20,14 @@ class Memu:
     def __init__(self):
         self.notebook = Notebook()
         self.choices = {
-            "1": self.show_notes,
+            "1": self.show_notes
             # "2": self.search_notes,
             # "3": self.add_note,
             # "4": self.modify_note,
             # "5": self.quit
         }
 
-    def display_memu(self):
+    def display_menu(self):
         print("""
         Notebook Memu
         
@@ -40,46 +40,42 @@ class Memu:
         5.Quit  
         """)
 
+    def run(self):
+        """Display the menu and repond to choices."""
+        while True:
+            self.display_menu()
+            choice = input("Enter an option: ")
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choice))
 
-def run(self):
-    """Display the menu and repond to choices."""
-    while True:
-        self.display_menu()
-        choice = input("Enter an option: ")
-        action = self.choices.get(choice)
-        if action:
-            action()
-        else:
-            print("{0} is not a valid choice".format(choice))
+    def show_notes(self, notes=None):
+        if not notes:
+            notes = self.notebook.notes
+        for note in notes:
+            print("{0}:{1}\n{2}".format(note.id, note.tags, note.memo))
 
+    def search_notes(self):
+        return None
 
-def show_notes(self, notes=None):
-    if not notes:
-        notes = self.notebook.notes
-    for note in notes:
-        print("{0}:{1}\n{2}".format(note.id, note.tags, note.memo))
+    def add_note(self):
+        return None
 
+    def modify_note(self):
+        return None
 
-def search_notes(self):
-    return None
-
-
-def add_note(self):
-    return None
-
-
-def modify_note(self):
-    return None
-
-
-def quit(self):
-    return None
-
-
-class Main():
-    def __init__(self):
-        pass
+    def quit(self):
+        return None
 
 
 if __name__ == "__main__":
     Memu().run()
+
+    # choice = input("Enter an option: ")
+    # choices = {
+    #     "1": "a",
+    #     "2": "b"
+    # }
+    # print(choices.get(choice))
